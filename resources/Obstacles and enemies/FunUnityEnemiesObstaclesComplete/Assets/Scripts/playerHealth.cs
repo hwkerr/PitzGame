c0c79077f4a53685ae85a1dd3e4c4d46c850779b
Logan Czarnecki <lczarne@clemson.edu>
2018-10-15 15:01:31 -0400

    Added Learning Resources

---
+++ resources/Obstacles and enemies/FunUnityEnemiesObstaclesComplete/Assets/Scripts/playerHealth.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class playerHealth : MonoBehaviour {
+
+	public AudioClip playerDamaged;
+	AudioSource playerAS;
+
+	public float fullHealth;
+	float currentHealth;
+
+	public GameObject playerDeathFX;
+
+	public Image healthSlider;
+	public Image damageIndicator;
+	public Text rubyCount;
+	public CanvasGroup endGameCanvas;
+	public Text EndGameText;
+
+	Color flashColour = new Color (255f, 255f, 255f, 0.5f);
+	float indicatorSpeed = 5f;
+
+	bool damaged;
+
+	//Player death
+	playerControllerScript controlMovement;
+
+	//Coin collection
+	int collectedRubies = 0;
+
+	// Use this for initialization
+	void Start () {
+		controlMovement = GetComponent<playerControllerScript> ();
+		rubyCount.text = collectedRubies.ToString();
+		currentHealth = fullHealth;
+		playerAS = GetComponent<AudioSource> ();
+		healthSlider.fillAmount = 0f;
+	}
+
+	void Update(){
+		if (damaged) {
+			damageIndicator.color = flashColour;
+		} else {
+			damageIndicator.color = Color.Lerp (damageIndicator.color, Color.clear, indicatorSpeed * Time.deltaTime);
+		}
+		damaged = false;
+	}
+
+	public void addDamage(float damage){
+		if (damage <= 0)
+			return;
+		currentHealth -= damage;
+
+		healthSlider.fillAmount = 1 - currentHealth / fullHealth;
+
+		playerAS.PlayOneShot (playerDamaged);
+
+		damaged = true;
+
+		if (currentHealth <= 0) {
+			makeDead ();
+		}
+	}
+		
+	public void makeDead(){
+		damageIndicator.color = flashColour;
+		Instantiate (playerDeathFX, transform.position, Quaternion.identity);
+		EndGameText.text = "You Died!";
+		winGame();
+		Destroy (gameObject);
+	}
+
+	public void addRuby(){
+		collectedRubies +=1;
+		rubyCount.text = collectedRubies.ToString();
+		if(collectedRubies>2){
+			EndGameText.text = "You Win!";
+			GetComponent<playerControllerScript>().toggleCanMove();
+			winGame();
+		}
+	}
+
+	public void winGame(){
+		endGameCanvas.alpha = 1f;
+		endGameCanvas.interactable = true;
+	}
+}
---
+++ resources/Physics/02/demos/Collisions/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float _speed;
+    private float _horizontal;
+    public float _jumpForce;
+    private Rigidbody2D _rb2d;
+
+    public Material _red;
+
+	// Use this for initialization
+    private void Start ()
+    {
+        _rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	private void Update ()
+    {
+       Movement();
+       Jumping();
+	}
+
+    private void Movement()
+    {
+        _horizontal = Input.GetAxis("Horizontal");
+        Vector3 Mov = new Vector2(_horizontal, 0f);
+        transform.position += Mov * _speed * Time.deltaTime;
+    }
+
+    private void Jumping()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            _rb2d.AddForce(new Vector2(0f, _jumpForce), ForceMode2D.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Cube"))
+        {
+            GetComponent<Renderer>().material = _red;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Cube"))
+        {
+            collision.gameObject.transform.localScale *= 0.5f;
+        }
+    }
+
+    
+}
