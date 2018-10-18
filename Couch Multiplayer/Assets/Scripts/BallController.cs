acf706d95f3a49b8c688e98ac0eb184700d06dc0
Logan Czarnecki <lczarne@clemson.edu>
2018-10-18 16:36:53 -0400

    added a basic kinda working unity project

---
+++ Couch Multiplayer/Assets/Scripts/BallController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.position.x < -9.46)
+        {
+            resetBall();
+        }
+        if (transform.position.x > 9.44)
+        {
+            resetBall();
+        }
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+
+    }
+    void resetBall()
+    {
+        this.transform.position = new Vector2(0, 5);
+        GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+    }
+}
