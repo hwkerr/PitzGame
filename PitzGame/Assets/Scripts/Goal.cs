996a5346f7c7b83f2b709a196ac598ff853e26a9
Derek Rodriguez <dwrodri@g.clemson.edu>
2018-11-18 19:00:24 -0500

    added score variable and getter to goals

---
+++ PitzGame/Assets/Scripts/Goal.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal: MonoBehaviour {
+
+    private int score; 
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Pickup"))  
+        {
+            this.score++;
+        }
+    }
+
+    public int getScore()
+    {
+        return this.score;
+    }
+
+}
