1734d60ca9b6b930cf59165628344e04f16a11a7
hwkerr <hwkerr@clemson.edu>
2018-11-28 14:27:56 -0500

    Added MenuValues Class
    Static class MenuValues added for setting which player (number) is using which character.

--- PitzGame/Assets/Scripts/GameManager.cs
+++ PitzGame/Assets/Scripts/GameManager.cs
@@ -7,31 +7,44 @@ public class GameManager : MonoBehaviour {
     private GameObject[] thePlayers = new GameObject[4];
     [SerializeField] private GameObject[] characterPrefabs;
 
-    public enum Character
+    public enum CharacterPrefab
     {
         MalePlayer,
         FemPlayer
     }
-    
-    // Use this for initialization
-	void Start () {
-        AddPlayer(1, Character.MalePlayer);
-        AddPlayer(2, Character.FemPlayer);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 
-    // @requires 1 <= playerNum <= 4
-    private void AddPlayer(int playerNum, Character character)
+    private void Awake()
     {
-        if (thePlayers[playerNum-1] != null)
-            Destroy(thePlayers[playerNum-1]);
-        thePlayers[playerNum-1] = Instantiate(characterPrefabs[(int)character]);
-        thePlayers[playerNum-1].GetComponent<DefaultPlayer>().playerNum = playerNum;
-        float xval = (playerNum * 4f) - 10f;
-        thePlayers[playerNum-1].GetComponent<Transform>().position = new Vector3(xval, -0.5f, 0f);
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Use this for initialization
+    void Start () {
+        AddPlayer(MenuValues.GetPlayer(1));
+        AddPlayer(MenuValues.GetPlayer(2));
+        AddPlayer(MenuValues.GetPlayer(3));
+        AddPlayer(MenuValues.GetPlayer(4));
+    }
+
+    // @Requires 1 <= playerNum <= 4
+    // @Ensures Instantiates a 'character' in the slot for player number 'playerNum'
+    public void AddPlayer(Player player)
+    {
+        if (player != null)
+        {
+            int playerNum = player.playerNum;
+            CharacterPrefab character;
+            if (player.character == Character.Fem)
+                character = CharacterPrefab.FemPlayer;
+            else //if (player.character == Character.Male)
+                character = CharacterPrefab.MalePlayer;
+
+            if (thePlayers[playerNum - 1] != null)
+                Destroy(thePlayers[playerNum - 1]);
+            thePlayers[playerNum - 1] = Instantiate(characterPrefabs[(int)character]);
+            thePlayers[playerNum - 1].GetComponent<DefaultPlayer>().playerNum = playerNum;
+            float xval = (playerNum * 4f) - 10f; // pick spawn position based on playerNum
+            thePlayers[playerNum - 1].GetComponent<Transform>().position = new Vector3(xval, -0.5f, 0f);
+        }
     }
 }
