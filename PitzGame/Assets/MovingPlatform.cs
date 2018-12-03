432e9962b4009f29e21837147dc4db885b8b8cdd
Logan Czarnecki <lczarne@clemson.edu>
2018-12-03 15:34:58 -0500

    fixed sticky wall and moving platform glitch

--- PitzGame/Assets/MovingPlatform.cs
+++ PitzGame/Assets/MovingPlatform.cs
@@ -7,16 +7,18 @@ public class MovingPlatform : MonoBehaviour {
     Rigidbody2D rb;
     Transform tf;
     bool movingRight;
-
+    Rigidbody2D rigidb;
+    float moveSpeed = 1f; 
 	// Use this for initialization
 	void Start () {
 
         Transform tf = GetComponent<Transform>();
+        rigidb = GetComponent<Rigidbody2D>();
         movingRight = true;
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    /*void Update () {
 
         tf = GetComponent<Transform>();
 		if(tf.position.x > 5)
@@ -33,16 +35,36 @@ public class MovingPlatform : MonoBehaviour {
         {
             tf.SetPositionAndRotation(new Vector3(tf.position.x + getMoveSpeed(), tf.position.y, 0f), tf.rotation);
         }
-	}
+	}*/
+    private void Update()
+    {
+        tf = GetComponent<Transform>();
+        rigidb = GetComponent<Rigidbody2D>();
+        if (tf.position.x > 5)
+        {
+            movingRight = false;
+            rigidb.velocity = new Vector2(getMoveSpeed(), 0f);
+        }
+        else if (GetComponent<Transform>().position.x < -5)
+        {
+            movingRight = true;
+            rigidb.velocity = new Vector2(getMoveSpeed(), 0f);
+        }
+        else
+        {
+            rigidb.velocity = new Vector2(getMoveSpeed(), 0f);
+        }
+    }
+
     float getMoveSpeed()
     {
         if (movingRight)
         {
-            return .05f;
+            return moveSpeed;
         }
         else
         {
-            return -.05f;
+            return -1 * moveSpeed;
         }
     }
 
